a2eafe6de5f126671c3db7471412f4889fe6b7ed
Raynna <andreas.henriksson96@gmail.com>
2024-12-05 21:21:02 +0100

    Upload

---
+++ ConsoleApp/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp;
+
+public class Book {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Lent { get; set; }
+
+    // Standard Constructor
+    public Book(string title, bool lent) {
+        Title = title;
+        Lent = lent;
+    }
+
+    public Book() {
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using Microsoft.Data.Sqlite;
+namespace ConsoleApp {
+    
+    class Program {
+    
+        static void Main(string[] args) {
+            try {
+                Sql.Connect();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+            MainInterface.Welcome();
+        }
+    }
+}
