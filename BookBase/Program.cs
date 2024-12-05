98a83c88644102f1bd09b934ebe900a8ea739ba3
Raynna <andreas.henriksson96@gmail.com>
2024-12-05 22:04:59 +0100

    reupload

---
+++ BookBase/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using BookBase.raynna.application;
+namespace BookBase {
+    
+    class Program {
+    
+        static void Main(string[] args) {
+            try {
+                Sql.Connect();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+            MainInterface.Welcome();
+        }
+    }
+}
---
+++ BookBase/src/raynna/application/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookBase.raynna.application;
+
+public class Book {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Lent { get; set; }
+
+    // Standard Constructor
+    public Book(string title, bool lent) {
+        Title = title;
+        Lent = lent;
+    }
+
+    public Book() {
+    }
+}
